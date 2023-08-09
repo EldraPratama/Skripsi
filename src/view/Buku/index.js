24d1f9afcdb1693fa372a528e5478d568cee656a
Eldra <eldra.pratama@wgs-id.com>
2023-08-09 17:10:41 +0700

    feat: update UI

--- src/view/Buku/index.js
+++ src/view/Buku/index.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import MerchantLayout from "../Layout/MerchantLayout";
 import styles from "../Layout/styles";
-
+import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 import {
   Box,
+  Button,
+  IconButton,
+  Stack,
   Paper,
   Table, 
   TableBody, 
@@ -15,12 +18,20 @@ import {
   TableRow 
 } from "@mui/material"; 
 
+import {
+  ZoomIn,
+  Edit,
+  Delete,
+} from '@mui/icons-material';
+
+
 const BukuPage = () => {
   const [data, setData] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     // Memanggil endpoint di server backend untuk mendapatkan data
-    axios.get('http://localhost:5000/api/data')
+    axios.get('http://localhost:5000/api/buku')
       .then((response) => {
         setData(response.data);
       })
@@ -30,35 +41,21 @@ const BukuPage = () => {
   }, []);
 
   const columns = [
-    { id: 'name', label: 'Name', minWidth: 170 },
-    { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
-    {
-      id: 'population',
-      label: 'Population',
-      minWidth: 170,
-      align: 'right',
-      format: (value) => value.toLocaleString('en-US'),
-    },
-    {
-      id: 'size',
-      label: 'Size\u00a0(km\u00b2)',
-      minWidth: 170,
-      align: 'right',
-      format: (value) => value.toLocaleString('en-US'),
-    },
-    {
-      id: 'density',
-      label: 'Density',
-      minWidth: 170,
-      align: 'right',
-      format: (value) => value.toFixed(2),
-    },
+    { id: 'No', label: 'No', minWidth: 50 },
+    { id: 'Judul', label: 'Judul', minWidth: 200 },
+    { id: 'Penulis', label: 'Penulis', minWidth: 100 },
+    { id: 'Kode', label: 'Kode Buku', minWidth: 100 },
+    { id: 'Stok Buku', label: 'Stok Buku', minWidth: 100 },
+    { id: 'Aksi', label: 'Aksi', minWidth: 150 },
+    // {
+    //   id: 'density',
+    //   label: 'Density',
+    //   minWidth: 170,
+    //   align: 'right',
+    //   format: (value) => value.toFixed(2),
+    // },
   ];
-  
-  function createData(name, code, population, size) {
-    const density = population / size;
-    return { name, code, population, size, density };
-  }
+
 
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
@@ -72,35 +69,20 @@ const BukuPage = () => {
     setPage(0);
   };
   
-  const rows = [
-    createData('India', 'IN', 1324171354, 3287263),
-    createData('China', 'CN', 1403500365, 9596961),
-    createData('Italy', 'IT', 60483973, 301340),
-    createData('United States', 'US', 327167434, 9833520),
-    createData('Canada', 'CA', 37602103, 9984670),
-    createData('Australia', 'AU', 25475400, 7692024),
-    createData('Germany', 'DE', 83019200, 357578),
-    createData('Ireland', 'IE', 4857000, 70273),
-    createData('Mexico', 'MX', 126577691, 1972550),
-    createData('Japan', 'JP', 126317000, 377973),
-    createData('France', 'FR', 67022000, 640679),
-    createData('United Kingdom', 'GB', 67545757, 242495),
-    createData('Russia', 'RU', 146793744, 17098246),
-    createData('Nigeria', 'NG', 200962417, 923768),
-    createData('Brazil', 'BR', 210147125, 8515767),
-  ];
-
   return (
     <MerchantLayout>
       <Box sx={styles.boxStyled}>
-        <div>
+        <Stack direction={"row"} justifyContent={"space-between"} marginBottom={2}>
           <h2>Data Buku</h2>
-          <ul>
-            {data.map((item) => (
-              <li key={item.id}>{item.name}</li>
-            ))}
-          </ul>
-        </div>
+          <Button
+            variant={"contained"}
+            color="primary"
+            style={{ fontWeight: "bold", borderRadius:"25px", marginTop:"30px" }}
+            onClick={() => navigate("/buku/tambah")}
+          >
+            Tambah
+          </Button>
+        </Stack>
         <Paper sx={{ width: '100%', overflow: 'hidden' }}>
           <TableContainer sx={{ maxHeight: 440 }}>
             <Table stickyHeader aria-label="sticky table">
@@ -109,7 +91,7 @@ const BukuPage = () => {
                   {columns.map((column) => (
                     <TableCell
                       key={column.id}
-                      align={column.align}
+                      align={"center"}
                       style={{ minWidth: column.minWidth }}
                     >
                       {column.label}
@@ -118,21 +100,31 @@ const BukuPage = () => {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {rows
+                {data
                   .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                  .map((row) => {
+                  .map((row, index) => {
                     return (
                       <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                        {columns.map((column) => {
-                          const value = row[column.id];
-                          return (
-                            <TableCell key={column.id} align={column.align}>
-                              {column.format && typeof value === 'number'
-                                ? column.format(value)
-                                : value}
-                            </TableCell>
-                          );
-                        })}
+                        <TableCell align="center">
+                          {index + 1}
+                        </TableCell>
+                        <TableCell align="center">
+                          {row.judul}
+                        </TableCell>
+                        <TableCell align="center">
+                          {row.penulis}
+                        </TableCell>
+                        <TableCell align="center">
+                          {row.kode_buku}
+                        </TableCell>
+                        <TableCell align="center">
+                          {row.stok_buku}
+                        </TableCell>
+                        <TableCell align="center">
+                          <IconButton color="primary"><ZoomIn/></IconButton>                      
+                          <IconButton color="success"><Edit/></IconButton>                      
+                          <IconButton color="error"><Delete/></IconButton>                      
+                        </TableCell>
                       </TableRow>
                     );
                   })}
@@ -142,7 +134,7 @@ const BukuPage = () => {
           <TablePagination
             rowsPerPageOptions={[10, 25, 100]}
             component="div"
-            count={rows.length}
+            count={data.length}
             rowsPerPage={rowsPerPage}
             page={page}
             onPageChange={handleChangePage}
--- src/view/Buku/tambah.js
+++ src/view/Buku/tambah.js
@@ -5,10 +5,20 @@ import styles from "../Layout/styles";
 import axios from 'axios';
 import {
   Box,
+  TextField,
+  Stack,
+  Button
 } from "@mui/material"; 
 
 const TambahBukuPage = () => {
   const [data, setData] = useState([]);
+  const [kode_buku, setKode_buku] = useState("");
+  const [judul, setJudul] = useState("");
+  const [penulis, setPenulis] = useState("");
+  const [penerbit, setPenerbit] = useState("");
+  const [tahun_terbit, setTahun_terbit] = useState(0);
+  const [harga_buku, setHarga_buku] = useState(0);
+  const [stok_buku, setStok_buku] = useState(0);
 
   useEffect(() => {
     // Memanggil endpoint di server backend untuk mendapatkan data
@@ -21,16 +31,123 @@ const TambahBukuPage = () => {
       });
   }, []);
 
+  const handleAddBuku = () => {
+    let body = {
+      kode_buku: kode_buku,
+      judul: judul,
+      penulis: penulis,
+      penerbit: penerbit,
+      tahun_terbit: Number(tahun_terbit),
+      harga_buku: Number(harga_buku),
+      stok_buku: Number(stok_buku),
+    }
+
+    axios.post('http://localhost:5000/api/buku', body)
+    .then((response) => {
+      setData(response.data);
+    })
+    .catch((error) => {
+      console.error('Error add data', error);
+    });
+
+  }
+
 
 
   return (
     <MerchantLayout>
       <Box sx={styles.boxStyled}>
-        <div>
-          <h2>Tambah Data Buku</h2>
-
-        </div>
-
+        <h2>Tambah Data Buku</h2>
+        <Stack direction="column" justifyContent={"center"} alignItems={"center"} marginTop={"30px"}>
+          <TextField 
+            id="outlined-basic" 
+            variant="outlined"
+            label="Kode Buku"
+            onChange={(e) => setKode_buku(e.target.value)}
+            sx={{
+              width:"450px",
+              margin:"10px",
+            }}
+          />
+          <TextField 
+            id="outlined-basic" 
+            variant="outlined"
+            label="Judul Buku"
+            onChange={(e) => setJudul(e.target.value)}
+            sx={{
+              width:"450px",
+              margin:"10px",
+            }}
+          />
+          <TextField 
+            id="outlined-basic" 
+            variant="outlined"
+            label="Penulis Buku"
+            onChange={(e) => setPenulis(e.target.value)}
+            sx={{
+              width:"450px",
+              margin:"10px",
+            }}
+          />
+          <TextField 
+            id="outlined-basic" 
+            variant="outlined"
+            label="Penerbit Buku"
+            onChange={(e) => setPenerbit(e.target.value)}
+            sx={{
+              width:"450px",
+              margin:"10px",
+            }}
+          />
+          <TextField 
+            id="outlined-basic" 
+            variant="outlined"
+            label="Tahun Terbit"
+            onChange={(e) => setTahun_terbit(e.target.value)}
+            sx={{
+              width:"450px",
+              margin:"10px",
+            }}
+          />
+          <TextField 
+            id="outlined-basic" 
+            variant="outlined"
+            label="Harga Buku"
+            onChange={(e) => setHarga_buku(e.target.value)}
+            sx={{
+              width:"450px",
+              margin:"10px",
+            }}
+          />
+          <TextField 
+            id="outlined-basic" 
+            variant="outlined"
+            label="Stok Buku"
+            onChange={(e) => setStok_buku(e.target.value)}
+            sx={{
+              width:"450px",
+              margin:"10px",
+            }}
+          />
+          <Stack direction="row" marginTop={"10px"}>
+            <Button
+              variant={"contained"}
+              color="primary"
+              style={{ fontWeight: "bold", marginRight: "200px" }}
+              onClick={() => handleAddBuku()}
+            >
+              Kembali
+            </Button>
+            <Button
+              variant={"contained"}
+              color="success"
+              style={{ fontWeight: "bold" }}
+              onClick={() => handleAddBuku()}
+            >
+              Tambah
+            </Button>
+          </Stack>
+        </Stack>
       </Box>
     </MerchantLayout>
   );
